99d5f8ef6949528de82718c2f615633858d5ec9c
AndrewHamel111 <46793311+AndrewHamel111@users.noreply.github.com>
2021-08-23 22:35:52 -0400

    True initcommit

---
+++ hamlib.c
@@ -0,0 +1,196 @@
+#include "hamlib.h"
+
+/**
+ * \file hamlib.c
+ * \author Andrew Hamel (me@andrewhamel.ca)
+ * \brief A small helper library developed to run along side Raylib.
+ * \version 0.1
+ * \date 2021-08-16
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+void DrawTextAligned(const char* text, int posX, int posY, int fontSize, Color color, enum TEXT_ALIGNMENT alignment)
+{
+	// no cases for TA_LEFT and TA_TOP as this is default behaviour in raylib.
+	int x = posX, y = posY;
+
+	if ((alignment & TA_CENTER) == TA_CENTER)
+	{
+		x -= MeasureText(text, fontSize)/2;
+	}
+	else if ((alignment & TA_RIGHT) == TA_RIGHT)
+	{
+		x -= MeasureText(text, fontSize);
+	}
+
+	if ((alignment & TA_MIDDLE) == TA_MIDDLE)
+	{
+		y -= fontSize/2;
+	}
+	else if ((alignment & TA_BOTTOM) == TA_BOTTOM)
+	{
+		y -= fontSize;
+	}
+
+	DrawText(text, x, y, fontSize, color);
+}
+
+bool VectorInRectangle(Vector2 v, Rectangle r)
+{
+	return (v.x < r.x + r.width) && (v.x > r.x) && (v.y < r.y + r.height) && (v.y > r.y);
+}
+
+float sqrDist(Vector2 a, Vector2 b)
+{
+	return sqr(a.x - b.x) + sqr(a.y - b.y);
+}
+
+float sqrDistEm(int x1, int y1, int x2, int y2)
+{
+	return sqr(x1 - x2) + sqr(y1 - y2);
+}
+
+Vector2 VecAdd(Vector2 a, Vector2 b)
+{
+	return (Vector2){a.x + b.x, a.y + b.y};
+}
+
+Vector2 VecMult(float a, Vector2 v)
+{
+	return (Vector2){a * v.x, a * v.y};
+}
+
+Vector2 VecDiff(Vector2 a, Vector2 b)
+{
+	return VecAdd(a, VecMult(-1, b));
+}
+
+bool VecEq(Vector2 a, Vector2 b)
+{
+	return a.x == b.x && a.y == b.y;
+}
+
+Vector2 GetCenter(Rectangle rect)
+{
+	return (Vector2){rect.x + rect.width/2, rect.y + rect.height/2};
+}
+
+////////////////
+// UTITLITIES //
+////////////////
+
+void swap(void* a, void* b, int sizeOf)
+{
+	void* temp = (void*)malloc(sizeOf);
+	memcpy(temp, a, sizeOf);
+	memcpy(a, b, sizeOf);
+	memcpy(b, temp, sizeOf);
+	free(temp);
+}
+
+///////////
+// COLOR //
+///////////
+
+Color randomcolor()
+{
+	return (Color){rand() % 256,rand() % 256,rand() % 256,255};
+}
+
+Color tintcolor(Color color, float amount)
+{
+	if (amount > 1.0f || amount < 0.0f)
+		amount = 0.5f;
+
+	unsigned char max = MAX(MAX(color.r,color.g),color.b);
+	bool getsDarker = max > 160;
+
+	if (amount == 1.0f) return getsDarker ? BLACK : WHITE;
+
+	if (color.r == max)
+	{
+		unsigned char del = LERP(color.r, getsDarker ? 0 : 255, amount);
+		color.g = CLAMP(del - (color.r - color.g), 0, 255);
+		color.b = CLAMP(del - (color.r - color.b), 0, 255);
+		return (Color){del, color.g, color.b, 255};
+	}
+	else if (color.g == max)
+	{
+		unsigned char del = LERP(color.g, getsDarker ? 0 : 255, amount);
+		color.r = CLAMP(del - (color.g - color.r), 0, 255);
+		color.b = CLAMP(del - (color.g - color.b), 0, 255);
+		return (Color){color.r, del, color.b, 255};
+	}
+	else
+	{
+		unsigned char del = LERP(color.b, getsDarker ? 0 : 255, amount);
+		color.r = CLAMP(del - (color.b - color.r), 0, 255);
+		color.g = CLAMP(del - (color.b - color.g), 0, 255);
+		return (Color){color.r, color.g, del, 255};
+	}
+}
+
+/////////////
+// BUTTONS //
+/////////////
+
+bool Button(Rectangle rect, Color color)
+{
+	DrawRectangleRec(rect, color);
+	return VectorInRectangle(GetMousePosition(), rect);
+}
+
+bool ButtonTintable(Rectangle rect, Color color)
+{
+	bool flag = VectorInRectangle(GetMousePosition(), rect);
+	DrawRectangleRec(rect, flag ? tintcolor(color, 0.6f) : color);
+	return flag;
+}
+
+////////////
+// ARRAYS //
+////////////
+
+void shuffle(void* array, int count, int sizeOf)
+{
+	for(int i = 0; i < count; i++)
+	{
+		int index = i * sizeOf;
+		int j = i + (rand() % (count - i));
+		int jndex = j * sizeOf;
+
+		swap(array + index, array + jndex, sizeOf);
+	}
+}
+
+void shufflerange(void* array, int count, int sizeOf, int start, int end)
+{
+	start = CLAMP(start, 0, count - 1);
+	end = CLAMP(end, 0, count - 1);
+
+	shuffle(array + start, end - start + 1, sizeOf);
+}
+
+/////////////
+// DRAWING //
+/////////////
+
+void DrawTextureScaled(Texture2D texture, Vector2 pos, Vector2 scaling, Color color)
+{
+	Rectangle src = (Rectangle){0, 0, texture.width, texture.height};
+	Rectangle dst = (Rectangle){pos.x + ((1.0f - scaling.x) * texture.width * 0.5f), pos.y + ((1.0f - scaling.y) * texture.height * 0.5f), scaling.x * texture.width, scaling.y * texture.height};
+	DrawTexturePro(texture, src, dst, (Vector2){0, 0}, 0, color);
+}
+
+////////////////////////////
+// RECTANGLES AND VECTORS //
+////////////////////////////
+
+Rectangle PureSource(Texture2D texture)
+{
+	return (Rectangle){0, 0, texture.width, texture.height};
+}
---
+++ hamlib.h
@@ -0,0 +1,241 @@
+#ifndef HAMLIB_H_
+#define HAMLIB_H_
+/**
+ * \file hamlib.h
+ * \author Andrew Hamel (me@andrewhamel.ca)
+ * \brief A small helper library developed to run along side Raylib.
+ * \version 0.1
+ * \date 2021-08-16
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
+#include "raylib.h"
+
+// macros
+/** \brief Square of a value. */
+#define sqr(A) ((A) * (A))
+/** \brief Minimum of two values. */
+#define MIN(A,B) ((A) > (B) ? (B) : (A))
+/** \brief Maximum of two values. */
+#define MAX(A,B) ((A) < (B) ? (B) : (A))
+/** \brief Linear-Interpolation between A and B by T. */
+#define LERP(A,B,T) ((B)*(T) + (A)*(1 - T))
+/** \brief Clamps T by A and B. Identity function when A <= T <= B. */
+#define CLAMP(T,A,B) ( (T) < (A) ? (A) : ( (T > B) ? (B) : (T) ) )
+/** \brief Similar to Clamp, but when T Exceeds B it returns to A and vice versa. */
+#define CYCLE(T,A,B) ( (T) < (A) ? (B) : ( (T > B) ? (A) : (T) ) )
+
+/**
+ * \brief An enum used to determine text alignment.
+ * \details This enum supports | operations to achieve multiple styles:
+ * i.e. TA_CENTER | TA_BOTTOM will have the text centered horizontally but
+ * draw upward from posY.
+ */
+enum TEXT_ALIGNMENT
+{
+	TA_LEFT = 1, /** Horizontal Alignment - all text right of posX */
+	TA_CENTER = 2, /** Horizontal Alignment - centered on posX */
+	TA_RIGHT = 4, /** Horizontal Alignment - all text left of posX */
+	TA_TOP = 8, /** Vertical Alignment - all text below posY */
+	TA_MIDDLE = 16, /** Vertical Alignment - Centered on posY*/
+	TA_BOTTOM = 32 /** Vertical Alignment - all text above posY*/
+};
+
+/**
+ * \brief Draws text with the specified alignment.
+ * 
+ * \param text Text to be drawn.
+ * \param posX X position of the text anchor.
+ * \param posY Y position of the text anchor.
+ * \param fontSize Height of the drawn text in pixels.
+ * \param color Color of the text.
+ * \param alignment Enum value describing the text alignment. Use | to specify a horizontal and vertical alignment.
+ */
+void DrawTextAligned(const char* text, int posX, int posY, int fontSize, Color color, enum TEXT_ALIGNMENT alignment);
+
+////////////////////
+// MATH UTILITIES //
+////////////////////
+
+/**
+ * \brief Determine if a Vector lands in a Region.
+ * 
+ * \param v Vector to check.
+ * \param r Region to check.
+ * \return true If v lies in r
+ * \return false If v does not lie in r
+ */
+bool VectorInRectangle(Vector2 v, Rectangle r);
+
+/**
+ * \brief Find the square distance between two vectors (fast)
+ * 
+ * \param a First vector
+ * \param b Second vector
+ * \return float Square dist. between two Vectors
+ */
+float sqrDist(Vector2 a, Vector2 b);
+
+/**
+ * \brief sqrDist Emplace
+ * 
+ * \param x1 
+ * \param y1 
+ * \param x2 
+ * \param y2
+ * \return float Square dist. between two Vectors
+ */
+float sqrDistEm(int x1, int y1, int x2, int y2);
+
+/**
+ * \brief Add two vectors
+ * 
+ * \param a 
+ * \param b 
+ * \return Vector2 Sum of Vectors
+ */
+Vector2 VecAdd(Vector2 a, Vector2 b);
+
+/**
+ * \brief Scale a vector
+ * 
+ * \param a Scalar
+ * \param v Vector
+ * \return (Vector2){a * v.x, a * v.y}
+ */
+Vector2 VecMult(float a, Vector2 v);
+
+/**
+ * \brief Additive inverse of VecAdd
+ * 
+ * \param a 
+ * \param b 
+ * \return a - b
+ */
+Vector2 VecDiff(Vector2 a, Vector2 b);
+
+/**
+ * \brief Equality of Vectors
+ * 
+ * \param a 
+ * \param b 
+ * \return true if the vectors are equal
+ * \return false 
+ */
+bool VecEq(Vector2 a, Vector2 b);
+
+/**
+ * \brief Get the Center object
+ * 
+ * \param rect Rectangle describing object bounds
+ * \return Vector2 Vector at the center of the object
+ */
+Vector2 GetCenter(Rectangle rect);
+
+////////////////
+// UTITLITIES //
+////////////////
+
+/**
+ * \brief Swaps two objects in-place.
+ * \details The caller should specify the two pointers, followed by a call to sizeof and one of the objects.
+ * Optionally, they can use sizeof() with the direct object type, or wrap this function with a custom.
+ * I.e., for object Object, they could write a Swap(Object* a, Object* b) { swap(a, b, sizeof(Object)); }
+ * \param a 
+ * \param b 
+ * \param sizeOf sizeOf objects to be swapped.
+ */
+void swap(void* a, void* b, int sizeOf);
+
+///////////
+// COLOR //
+///////////
+
+/**
+ * \brief Generates a random color
+ * 
+ * \return A Random Color
+ */
+Color randomcolor(void);
+
+/** \fn Color tintcolor(Color color, float amount)
+* Tints a color by a normalized amount.
+* \param color Color to be tinted.
+* \param amount Normalized value. 0.0f returns color, 1.0f returns WHITE or BLACK.
+* \returns Tinted color.
+*/
+Color tintcolor(Color color, float amount);
+
+/////////////
+// BUTTONS //
+/////////////
+
+/**
+ * \brief A function for a mouse-based button.
+ * 
+ * \param rect Rectangle describing the button
+ * \param color Color the button should be drawn in.
+ * \return true when the mouse is highlighting the button.
+ */
+bool Button(Rectangle rect, Color color);
+
+/**
+ * \brief A function for a mouse-based button.
+ * 
+ * \param rect Rectangle describing the button
+ * \param color Color the button should be drawn in. When highlighted this color is also tinted.
+ * \return true when the mouse is highlighting the button.
+ */
+bool ButtonTintable(Rectangle rect, Color color);
+
+////////////
+// ARRAYS //
+////////////
+
+/**
+ * \brief A primitive shuffle algorithm. srand() must be set for good results.
+ * 
+ * \param array Pointer to array to be shuffled.
+ * \param count Length of array.
+ * \param sizeOf sizeof(ObjectInArray)
+ */
+void shuffle(void* array, int count, int sizeOf);
+
+/**
+ * \brief A primitive shuffle algorithm. Operates in a specified range.
+ * 
+ * \param array Pointer to array to be shuffled.
+ * \param count Length of array.
+ * \param sizeOf sizeof(ObjectInArray)
+ * \param start starting index of the shuffle range (inclusive).
+ * \param end ending index of the shuffle range (inclusive).
+ */
+void shufflerange(void* array, int count, int sizeOf, int start, int end);
+
+/////////////
+// DRAWING //
+/////////////
+
+/**
+ * \brief Draws a texture using DrawTexturePro.
+ * 
+ * \param texture Texture to draw
+ * \param pos Position to draw at
+ * \param scaling X and Y scaling to use
+ */
+void DrawTextureScaled(Texture2D texture, Vector2 pos, Vector2 scaling, Color color);
+
+////////////////////////////
+// RECTANGLES AND VECTORS //
+////////////////////////////
+
+/**
+ * \brief Returns a Source Rectangle of the full texture.
+ * \details Used best for functions like DrawTexturePro that require a source rectangle even when you don't necessarily need it, for example, for non-atlassed sprites.
+ * \param texture The texture to get a pure source from. 
+ * \return Rectangle A source rectangle with x,y = 0,0 and width,height matching the image's size.
+ */
+Rectangle PureSource(Texture2D texture);
+
+#endif
